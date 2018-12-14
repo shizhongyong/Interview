536861f814d5ca3dffe79c820c2f540d6584e9da
史忠勇 <46428886@qq.com>
2018-12-14 10:14:40 +0800

    增加gesture

--- app/src/main/java/com/shizy/interview/components/activity/MainActivity.java
+++ app/src/main/java/com/shizy/interview/components/activity/MainActivity.java
@@ -2,7 +2,10 @@ package com.shizy.interview.components.activity;
 
 import android.app.ListActivity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -12,9 +15,16 @@ import com.shizy.interview.components.activity.anim.CardFlipActivity;
 import com.shizy.interview.components.activity.anim.CrossfadeActivity;
 import com.shizy.interview.components.activity.anim.ZoomActivity;
 import com.shizy.interview.components.activity.drag.ViewDragActivity;
+import com.shizy.interview.components.activity.gesture.GestureBuilderActivity;
 import com.shizy.interview.components.activity.launch_mode.LaunchModeActivity;
+import com.shizy.interview.util.PermissionUtil;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends ListActivity {
+
+	private static final int RC_PERMISSION = 1;
 
 	private static final Class[] CLASS_LIST = new Class[] {
 			LaunchModeActivity.class,
@@ -26,14 +36,40 @@ public class MainActivity extends ListActivity {
 			CardFlipActivity.class,
 			ZoomActivity.class,
 			BottomSheetActivity.class,
-			ViewDragActivity.class
+			ViewDragActivity.class,
+			GestureBuilderActivity.class
 	};
+
+	@Override
+	public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+		for (int i = 0; i < permissions.length; i++) {
+			if (grantResults[i] != PackageManager.PERMISSION_GRANTED && PermissionUtil.isRuntime(this, permissions[i])) {
+
+				return;
+			}
+		}
+
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
 		setListAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, CLASS_LIST));
+
+		List<String> permissions = PermissionUtil.getPermissions(this);
+		List<String> denied = new ArrayList<>();
+		for (String permission : permissions) {
+			if (!PermissionUtil.isGranted(this, permission)) {
+				denied.add(permission);
+			}
+		}
+
+		if (denied.size() > 0) {
+			String[] deniedPermissions = new String[denied.size()];
+			denied.toArray(deniedPermissions);
+			ActivityCompat.requestPermissions(this, deniedPermissions, RC_PERMISSION);
+		}
 	}
 
 	@Override
---
+++ app/src/main/java/com/shizy/interview/util/PermissionUtil.java
@@ -0,0 +1,84 @@
+package com.shizy.interview.util;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.content.pm.PermissionInfo;
+import android.os.Build;
+import android.support.v4.content.ContextCompat;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static android.content.pm.PackageManager.GET_META_DATA;
+
+public class PermissionUtil {
+
+	/**
+	 * Return the permissions used in application.
+	 *
+	 * @return the permissions used in application
+	 */
+	public static List<String> getPermissions(Context context) {
+		return getPermissions(context, context.getPackageName());
+	}
+
+	/**
+	 * Return the permissions used in application.
+	 *
+	 * @param packageName The name of the package.
+	 * @return the permissions used in application
+	 */
+	public static List<String> getPermissions(Context context, final String packageName) {
+		PackageManager pm = context.getPackageManager();
+		try {
+			return Arrays.asList(
+					pm.getPackageInfo(packageName, PackageManager.GET_PERMISSIONS)
+							.requestedPermissions
+			);
+		} catch (PackageManager.NameNotFoundException e) {
+			e.printStackTrace();
+			return Collections.emptyList();
+		}
+	}
+
+	/**
+	 * Return whether <em>you</em> have granted the permissions.
+	 *
+	 * @param permissions The permissions.
+	 * @return {@code true}: yes<br>{@code false}: no
+	 */
+	public static boolean isGranted(Context context, final String... permissions) {
+		for (String permission : permissions) {
+			if (!isGranted(context, permission)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	private static boolean isGranted(Context context, final String permission) {
+		return Build.VERSION.SDK_INT < Build.VERSION_CODES.M
+				|| PackageManager.PERMISSION_GRANTED
+				== ContextCompat.checkSelfPermission(context, permission);
+	}
+
+	public static boolean isRuntime(Context context, String permission) {
+		PermissionInfo permissionInfo = getPermissionInfo(context, permission);
+		if (permissionInfo != null) {
+			return (permissionInfo.protectionLevel & PermissionInfo.PROTECTION_MASK_BASE)
+					== PermissionInfo.PROTECTION_DANGEROUS;
+		}
+		return false;
+	}
+
+	public static PermissionInfo getPermissionInfo(Context context, String permission) {
+		PackageManager pm = context.getPackageManager();
+		try {
+			return pm.getPermissionInfo(permission, GET_META_DATA);
+		} catch (PackageManager.NameNotFoundException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+}
